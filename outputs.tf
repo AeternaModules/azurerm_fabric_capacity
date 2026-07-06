79497d888c5e4d10ad31f298b6e45e314eb0a29b
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,28 @@ output "fabric_capacities" {
   description = "All fabric_capacity resources"
   value       = azurerm_fabric_capacity.fabric_capacities
 }
+output "fabric_capacities_administration_members" {
+  description = "List of administration_members values across all fabric_capacities"
+  value       = [for k, v in azurerm_fabric_capacity.fabric_capacities : v.administration_members]
+}
+output "fabric_capacities_location" {
+  description = "List of location values across all fabric_capacities"
+  value       = [for k, v in azurerm_fabric_capacity.fabric_capacities : v.location]
+}
+output "fabric_capacities_name" {
+  description = "List of name values across all fabric_capacities"
+  value       = [for k, v in azurerm_fabric_capacity.fabric_capacities : v.name]
+}
+output "fabric_capacities_resource_group_name" {
+  description = "List of resource_group_name values across all fabric_capacities"
+  value       = [for k, v in azurerm_fabric_capacity.fabric_capacities : v.resource_group_name]
+}
+output "fabric_capacities_sku" {
+  description = "List of sku values across all fabric_capacities"
+  value       = [for k, v in azurerm_fabric_capacity.fabric_capacities : v.sku]
+}
+output "fabric_capacities_tags" {
+  description = "List of tags values across all fabric_capacities"
+  value       = [for k, v in azurerm_fabric_capacity.fabric_capacities : v.tags]
+}
 
