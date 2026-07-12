76b6aa58e9effdf32c24b66db4aa70ef7531c40c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "fabric_capacities" {
-  description = "All fabric_capacity resources"
-  value       = azurerm_fabric_capacity.fabric_capacities
-}
 output "fabric_capacities_administration_members" {
-  description = "List of administration_members values across all fabric_capacities"
-  value       = [for k, v in azurerm_fabric_capacity.fabric_capacities : v.administration_members]
+  description = "Map of administration_members values across all fabric_capacities, keyed the same as var.fabric_capacities"
+  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.administration_members }
 }
 output "fabric_capacities_location" {
-  description = "List of location values across all fabric_capacities"
-  value       = [for k, v in azurerm_fabric_capacity.fabric_capacities : v.location]
+  description = "Map of location values across all fabric_capacities, keyed the same as var.fabric_capacities"
+  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.location }
 }
 output "fabric_capacities_name" {
-  description = "List of name values across all fabric_capacities"
-  value       = [for k, v in azurerm_fabric_capacity.fabric_capacities : v.name]
+  description = "Map of name values across all fabric_capacities, keyed the same as var.fabric_capacities"
+  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.name }
 }
 output "fabric_capacities_resource_group_name" {
-  description = "List of resource_group_name values across all fabric_capacities"
-  value       = [for k, v in azurerm_fabric_capacity.fabric_capacities : v.resource_group_name]
+  description = "Map of resource_group_name values across all fabric_capacities, keyed the same as var.fabric_capacities"
+  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.resource_group_name }
 }
 output "fabric_capacities_sku" {
-  description = "List of sku values across all fabric_capacities"
-  value       = [for k, v in azurerm_fabric_capacity.fabric_capacities : v.sku]
+  description = "Map of sku values across all fabric_capacities, keyed the same as var.fabric_capacities"
+  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.sku }
 }
 output "fabric_capacities_tags" {
-  description = "List of tags values across all fabric_capacities"
-  value       = [for k, v in azurerm_fabric_capacity.fabric_capacities : v.tags]
+  description = "Map of tags values across all fabric_capacities, keyed the same as var.fabric_capacities"
+  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.tags }
 }
 
