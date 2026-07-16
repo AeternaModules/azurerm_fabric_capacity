a6c2eafdfa2e9fd359bcfb065a7d3080dd615f10
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:48:08 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "fabric_capacities_id" {
   description = "Map of id values across all fabric_capacities, keyed the same as var.fabric_capacities"
-  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.id }
+  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "fabric_capacities_administration_members" {
   description = "Map of administration_members values across all fabric_capacities, keyed the same as var.fabric_capacities"
-  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.administration_members }
+  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.administration_members if v.administration_members != null && length(v.administration_members) > 0 }
 }
 output "fabric_capacities_location" {
   description = "Map of location values across all fabric_capacities, keyed the same as var.fabric_capacities"
-  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.location }
+  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "fabric_capacities_name" {
   description = "Map of name values across all fabric_capacities, keyed the same as var.fabric_capacities"
-  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.name }
+  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "fabric_capacities_resource_group_name" {
   description = "Map of resource_group_name values across all fabric_capacities, keyed the same as var.fabric_capacities"
-  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.resource_group_name }
+  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "fabric_capacities_sku" {
   description = "Map of sku values across all fabric_capacities, keyed the same as var.fabric_capacities"
-  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.sku }
+  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.sku if v.sku != null && length(v.sku) > 0 }
 }
 output "fabric_capacities_tags" {
   description = "Map of tags values across all fabric_capacities, keyed the same as var.fabric_capacities"
-  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.tags }
+  value       = { for k, v in azurerm_fabric_capacity.fabric_capacities : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 
